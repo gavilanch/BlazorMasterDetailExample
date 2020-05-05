21ffacb30076da4e64662769196ead47708a7238
Felipe Gavilan <elmejor_887@hotmail.com>
2020-05-05 10:17:34 -0400

    Initial Commit

---
+++ BlazorMasterDetail/Server/Controllers/StudentsController.cs
@@ -0,0 +1,38 @@
+﻿using BlazorMasterDetail.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMasterDetail.Server.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> Get()
+        {
+            return await context.Students.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Student student)
+        {
+            context.Students.Add(student);
+            await context.SaveChangesAsync();
+            return student.Id;
+        }
+    }
+}
+
+
---
+++ BlazorMasterDetail/Shared/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMasterDetail.Shared
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
